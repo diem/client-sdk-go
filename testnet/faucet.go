3e3cabadc0004aadd16bd36041d2b56dac3720d7
Xiao Li <ilx@fb.com>
2020-08-21 17:24:15 -0700

    create child vasp account example

--- testnet/faucet.go
+++ testnet/faucet.go
@@ -13,15 +13,17 @@ import (
 )
 
 // MustMint mints coins with retry, and panics if all retries failed.
-func MustMint(authKey string, amount uint64, currencyCode string) (ret int) {
+// This func also wait for next account seq.
+func MustMint(authKey string, amount uint64, currencyCode string) {
 	retry := 3
 	var err error
 	for i := 0; i < retry; i++ {
-		ret, err = Mint(authKey, amount, currencyCode)
+		ret, err := Mint(authKey, amount, currencyCode)
 		if err != nil {
 			time.Sleep(1100 * time.Millisecond)
 			continue
 		}
+		waitAccountSequence(ret)
 		return
 	}
 	panic(fmt.Sprintf("mint coins failed with retry: %s", err.Error()))
@@ -45,4 +47,18 @@ func Mint(authKey string, amount uint64, currencyCode string) (int, error) {
 		return 0, fmt.Errorf("Non 200 response: %s", string(body))
 	}
 	return strconv.Atoi(string(body))
+}
+
+func waitAccountSequence(seq int) {
+	for i := 0; i < 100; i++ {
+		account, err := Client.GetAccount(DDAcountAddress)
+		if err != nil {
+			panic(fmt.Errorf("get account failed: %v", err))
+		}
+		if account.SequenceNumber >= uint64(seq) {
+			return
+		}
+		time.Sleep(100 * time.Millisecond)
+	}
+	panic("waiting for mint timeout")
 }

e369463f17a7b199ae3fccde43bd8175f8235569
Xiao Li <ilx@fb.com>
2020-09-22 17:08:09 -0700

    add doc.go for adding summary doc to packages

---
+++ stdlib/doc.go
@@ -0,0 +1,5 @@
+// Copyright (c) The Libra Core Contributors
+// SPDX-License-Identifier: Apache-2.0
+
+// Provides Libra move language stdlib script encoding and decoding utilities.
+package stdlib
---
+++ txnmetadata/doc.go
@@ -0,0 +1,6 @@
+// Copyright (c) The Libra Core Contributors
+// SPDX-License-Identifier: Apache-2.0
+
+// Provides utility functions for creating peer to peer transaction metadata
+// (https://github.com/libra/lip/blob/master/lips/lip-4.md).
+package txnmetadata
